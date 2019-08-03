2d8926ef63ecc29fba59754beed3f8f75ba0a967
Pedro Neto <pedromneto97@gmail.com>
2019-08-03 17:33:31 -0300

    Add: Classe para controle da iluminação

---
+++ Light.py
@@ -0,0 +1,19 @@
+from Actuator import Actuator
+
+
+class Light(Actuator):
+
+    def __init__(self, led: int, button: int, state: bool = False) -> None:
+        from gpiozero import LED, Button
+        self._light = LED(led)
+        self._button = Button(button, pull_up=False)
+        self._state = state
+
+    def activate(self) -> None:
+        self._light.blink(off_time=0, n=1)
+
+    def deactivate(self) -> None:
+        self._light.blink(off_time=0, n=1)
+
+    def change_state(self) -> None:
+        self._state = not self._state
