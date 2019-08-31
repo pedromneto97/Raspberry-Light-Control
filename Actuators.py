cc76e62ae3d0ad7b52ae65078c721464fd74507b
Pedro Neto <pedromneto97@gmail.com>
2019-08-31 20:58:55 -0300

    Update: Corrige tipo e função retornando as ids e o callback para registrar

--- Actuators.py
+++ Actuators.py
@@ -1,4 +1,5 @@
-from typing import Dict, List, Iterator
+from collections import namedtuple
+from typing import Dict, List, Callable, Tuple
 
 from Models.Actuator import Actuator
 from Models.Light import Light
@@ -6,11 +7,15 @@ from Models.Light import Light
 
 def actuator(item: Dict):
     get = item.get
-    if get('TYPE') is "LIGHT":
+    if get('type') == "LIGHT":
         return Light(light=get('OUTPUT'), button=get('INPUT'), _id=get('_id'))
     raise Exception("Invalid type")
 
 
 class Actuators:
     def __init__(self, items: List[Dict]) -> None:
-        self.actuators: Iterator[Actuator] = map(actuator, items)
+        self.actuators: List[Actuator] = [actuator(item) for item in items]
+
+    def on_join(self) -> List[Tuple[str, Callable]]:
+        Register = namedtuple('Register', ['id', 'callback'])
+        return [Register(id=act.get_id(), callback=act.change_state) for act in self.actuators]
